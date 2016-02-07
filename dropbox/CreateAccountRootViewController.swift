94a5cf7a7d255f8a97fe187bce3db2b0161026ce
Tina Chen <tinatxchen@gmail.com>
2016-02-06 19:05:09 -0800

    Required features and ability to favorite a file

---
+++ dropbox/CreateAccountRootViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CreateAccountRootViewController.swift
+//  dropbox
+//
+//  Created by Tina Chen on 2/4/16.
+//  Copyright © 2016 tinachen. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        performSegueWithIdentifier("firstSegue", sender: self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
